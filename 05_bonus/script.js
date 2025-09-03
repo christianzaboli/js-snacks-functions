0410f63892411c0c709943ac7925018a370c5996
christianzaboli <zabolichristian@gmail.com>
2025-09-03 16:49:50 +0200

    Step 05 bonus completato

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,44 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+// funzione richiesta dal quesito di base
+function timedSalute(x) {
+    let saluteIf = ''
+    const day = new Date();
+    let hour = day.getHours();
+    if (hour > 3 && hour < 13) {
+        saluteIf = 'Buongiorno ' + x + '.'
+    } else if (hour >= 13 || hour <= 17) {
+        saluteIf = 'Buon pomeriggio ' + x + '.'
+    } else if (hour > 17) {
+        saluteIf = 'Buonasera ' + x + '.'
+    }
+    console.log(hour);
+    
+    return saluteIf
+}
 
+// funzione alternativa per il caso ore 18
+function timedSaluteALT(x) {
+    let saluteIf = ''
+    const day = new Date("March 13, 08 02:20");
+    let hour = day.getHours();
+    if (hour > 3 && hour < 13) {
+        saluteIf = 'Buongiorno ' + x + '.'
+    } else if (hour >= 13 && hour <= 17) {
+        saluteIf = 'Buon pomeriggio ' + x + '.'
+    } else if (hour > 17 || hour <= 3) {
+        saluteIf = 'Buonasera ' + x + '.'
+    }
+    console.log(hour);
+    
+    return saluteIf
+}
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(timedSalute(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+console.log(timedSaluteALT(name));
