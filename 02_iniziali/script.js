b8d5f652820ae88837b3805e20a3daae8b2b384f
christianzaboli <zabolichristian@gmail.com>
2025-09-03 15:04:04 +0200

    Step 02 iniziali completato

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,10 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstLettersOf(array) {
+    // Creo il nuovo array dove verranno pushate le lettere
+    const newArray = []
+    // Cerco le prime lettere degli elementi dell'array selezionato
+    for (let i = 0; i < array.length; i++) {
+        const elements = array[i];
+        newArray.push(elements.charAt())
+
+        console.log(elements, newArray);
+    }
+    return newArray
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const initialsNames = firstLettersOf(names)
+console.log(newwarray);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
