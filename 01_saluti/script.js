a56efeac8cb9153bb7281da2032797e64fffad2b
christianzaboli <zabolichristian@gmail.com>
2025-09-03 14:42:42 +0200

    Step 01 saluti completato

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,8 +4,15 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+function salute(x) {
+    const saluteX = "ciao " + x;
+    return saluteX
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const welcome = salute(userName);
+console.log(welcome);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
