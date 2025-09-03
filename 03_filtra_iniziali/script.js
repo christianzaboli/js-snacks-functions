65f2b86b83bc3fa68aecb773a2d0b38c15e31768
christianzaboli <zabolichristian@gmail.com>
2025-09-03 15:20:13 +0200

    Step 03 filtra iniziali completato

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -6,8 +6,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+function firstLettersFilter(array, x) {
+    // Creo il nuovo array dove verranno pushate le parole filtrate per la lettera scelta
+    const newArray = []
+    // Cerco le prime lettere degli elementi dell'array selezionato
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (element.charAt() === x) {
+            newArray.push(element)
+        }
+    }
+    return newArray
+}
+
+
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let letterToFilter = 'A' //lettera da scegliere per il filtro
+const namesA = firstLettersFilter(names, letterToFilter)
+console.log(namesA);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
